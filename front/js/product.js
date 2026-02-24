9e36bfd7ff053505120fb40a40c22fff0633760e
gaelramos-ship-i <gael.ramos@laplateforme.io>
2026-02-24 16:57:46 +0100

    product elements

--- front/js/product.js
+++ front/js/product.js
@@ -9,29 +9,45 @@ async function getGeniArt() {
 
     let data = await req.json();
 
-    const article = 
-    `<article>
-        <figure>
-            <img src="${data.image}" alt="${data.shorttitle}">
-        </figure>
-        <div>
-            <h1>${data.titre}</h1>
-            <p>Plongez dans l'univers mystique de 'Bird', une œuvre d'art captivante quitranscende les limites de la réalité. Réalisée dans le style éthéré et spectral,cette pièce évoque la présence d'un oiseau envoûtant qui semble flotter dansl'au-delà. </p>
-            <div class="price">
-                <p>Acheter pour</p>
-                <span class="showprice">35.25€</span>
-            </div>
-            <div class="declinaison">
-                <input type="number" name="quantity" id="quantity" placeholder="1" value="1" minlength="1">
-                <select name="format" id="format">
-                </select>
-            </div>
-            <a class="button-buy" href="#">Buy bird</a>
-        </div>
-    </article>`
-    const detailoeuvre = document.querySelector('.detailoeuvre');
-    detailoeuvre.insertAdjacentHTML('beforeend', article);
-    
+    const img = document.querySelector('figure img')
+    img.src = data.image 
+    img.alt = data.shorttitle
+
+    const h1 = document.querySelector('div h1')
+    h1.textContent = data.titre
+
+    const p = document.querySelector('aside p')
+    p.textContent = data.description
+
+    const shortp = document.querySelector('div p')
+    shortp.textContent = data.description.substr(0, 244);
+
+    const a = document.querySelector('.button-buy')
+    a.textContent = data.shorttitle
+
+    // const article = 
+    // `<h2>Description de l’oeuvre :  ${data.titre}</h2>
+    // <article>
+    //     <figure>
+    //         <img src="${data.image}" alt="${data.shorttitle}">
+    //     </figure>
+    //     <div>
+    //         <h1>${data.titre}</h1>
+    //         <p>${data.description}</p>
+    //         <div class="price">
+    //             <p>Acheter pour</p>
+    //             <span class="showprice">35.25€</span>
+    //         </div>
+    //         <div class="declinaison">
+    //             <input type="number" name="quantity" id="quantity" placeholder="1" value="1" minlength="1">
+    //             <select name="format" id="format">
+    //             </select>
+    //         </div>
+    //         <a class="button-buy" href="#">Buy ${data.shorttitle}</a>
+    //     </div>
+    // </article>`
+    // const detailoeuvre = document.querySelector('.detailoeuvre');
+    // detailoeuvre.insertAdjacentHTML('beforeend', article);
     
   } catch (e) {
     console.error("Une erreur s'est produite : ", e);
